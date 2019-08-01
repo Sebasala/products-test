17f73985a641141738655f1b05ae5f7c56a2d6f1
Sebastian Salazar <sebastian@lacaja.click>
2019-08-01 05:53:43 -0500

    react-md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Team-International!</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit exercitationem laboriosam est cum voluptatum assumenda animi dignissimos, vitae voluptatem sapiente omnis quia, eos minima consequuntur perferendis culpa mollitia error harum?</p>
     </div>
   );
 }
