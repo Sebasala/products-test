8093dae2f049029b6314033324d2c793c8e6d5a7
Sebastian Salazar <sebastian@lacaja.click>
2019-08-01 06:45:46 -0500

    material-design-icons

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hello Team-International!</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit exercitationem laboriosam est cum voluptatum assumenda animi dignissimos, vitae voluptatem sapiente omnis quia, eos minima consequuntur perferendis culpa mollitia error harum?</p>
+      <i className="material-icons">face</i>
     </div>
   );
 }
